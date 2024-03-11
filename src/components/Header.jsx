4b440b50ea8b7e4fa9db07c1bb272a53790a3b3d
lebohang_mdm <lebohangmdm@gmail.com>
2024-03-11 19:04:53 +0200

    search weather by location

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import Weather from "./Weather";
 import useGeolocation from "../hooks/useGeolocation";
+import Search from "./Search";
 
 const Header = () => {
   const [weatherData, setWeatherData] = useState({});
+  const [query, setQuery] = useState("");
   const location = useGeolocation();
   const {
     coordinates: { lat, lng },
@@ -30,12 +32,12 @@ const Header = () => {
 
   return (
     <header className="bg-red-200 w-[900px] mx-auto p-8 flex justify-between">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-4xl">Today checklist⛅</h1>
-        <input
-          type="text"
-          className="py-1 px-4 rounded-xl w-[350px]  focus:outline-none focus:ring-2 focus:ring-yellow-400 border-none"
-          placeholder="Enter today's location"
+      <div className="flex flex-col gap-3">
+        <h1 className="text-5xl">Today checklist⛅</h1>
+        <Search
+          query={query}
+          setQuery={setQuery}
+          setWeatherData={setWeatherData}
         />
       </div>
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,38 @@
+const Search = ({ query, setQuery, setWeatherData }) => {
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (query.length < 3) return;
+
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${
+          import.meta.env.VITE_API_KEY
+        }`
+      );
+
+      if (!res.ok)
+        throw new Error("We cannot find the weather data for this location");
+
+      const data = await res.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setQuery("");
+    }
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        className="py-2 px-4 rounded-xl w-[350px]  focus:outline-none focus:ring-2 focus:ring-yellow-400 border-none"
+        placeholder="Enter your location today"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+};
+
+export default Search;

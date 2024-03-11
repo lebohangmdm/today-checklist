da39a4ab85afeda7e9d3c7457c2a851ecfffa937
lebohang_mdm <lebohangmdm@gmail.com>
2024-03-11 19:47:34 +0200

    add items functionality

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 const Item = ({ item }) => {
-  const { name } = item;
+  const { name, packed } = item;
 
   return (
-    <div className="flex items-center space-x-8">
+    <div className="flex items-center space-x-8 ">
       <p className="bg-white py-2 px-4 w-full ">{name}</p>
       <div className="flex  items-center gap-2">
-        <input type="checkbox" name="che" id="" />
+        <input type="checkbox" name="checkbox" id="" />
         <button className="text-xl text-red-500">
           <ion-icon name="trash-outline"></ion-icon>
         </button>
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,42 +1,47 @@
+import { useState } from "react";
 import Item from "./Item";
 
 const Items = () => {
-  const list = [
-    {
-      id: 1,
-      name: "umbrella",
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) return;
+    const id = Date.now();
+    const newItem = {
+      id,
+      name,
       packed: false,
-    },
-    {
-      id: 2,
-      name: "hat",
-      packed: false,
-    },
-    {
-      id: 1,
-      name: "cap",
-      packed: true,
-    },
-  ];
+    };
+    setItems([...items, newItem]);
+    setName("");
+  };
 
   return (
     <section className="bg-blue-200 w-[900px] px-8 py-4">
       <div className="flex justify-center mb-4">
         <h2 className="text-2xl text-center">Add your items for today✍️</h2>
       </div>
-      <form className="flex justify-center">
+      <form className="flex justify-center" onSubmit={handleSubmit}>
         <input
           type="text"
-          className="py-1 px-4 w-[300px] focus:outline-none focus:ring-2 ring-blue-400 "
+          className="py-1 px-4 w-[300px] focus:outline-none focus:ring-2 ring-blue-400"
           placeholder="Enter your item"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <button className="py-1 px-4 bg-blue-600">Add Item</button>
       </form>
 
-      <div className="mt-8 space-y-4">
-        {list.map((item) => {
+      <div className="mt-8 space-y-3">
+        {items.map((item) => {
           return <Item key={item.id} item={item} />;
         })}
+      </div>
+      <div className="text-center mt-12">
+        <button className="bg-red-600 py-2 px-4 text-white ">Clear List</button>
       </div>
     </section>
   );
